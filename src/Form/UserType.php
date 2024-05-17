6e24a9053c8e565e837fd69be3a4384cfd70759c
EUKao <corenthin.oillic44@gmail.com>
2024-05-17 14:05:53 +0200

    edit datatransformer

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,16 +4,14 @@ namespace App\Form;
 
 use App\Entity\Role;
 use App\Entity\User;
-use App\Form\DataTransformer\RoleToLabelTransformer;
+use App\Form\RoleToLabelTransformer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
